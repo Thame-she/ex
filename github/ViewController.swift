a3e55abf0536674831dd9a1e83c2087f26016058
MAC <mac@MACui-Mac-mini.local>
2016-11-20 22:06:02 +0900

    Initial Commit

---
+++ github/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  github
+//
+//  Created by MAC on 2016. 11. 20..
+//  Copyright © 2016년 MAC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
